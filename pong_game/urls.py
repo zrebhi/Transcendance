2ca8ca4546120bc3dddb550131bfc0a8e51ee8ef
zrebhi <zakaria.rebhi@gmail.com>
2024-01-01 22:11:18 +0100

    Pong game running with nginx

--- pong_game/urls.py
+++
@@ -1,9 +0,0 @@
-# pong_game/urls.py
-
-from django.urls import path
-from .views import pong_game_view
-
-urlpatterns = [
-    path('pong/', pong_game_view, name='pong_game'),
-    # Add more URL patterns if needed
-]
--- pong_game/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def pong_game_view(request):
-    return render(request, 'pong_template.html')
-
