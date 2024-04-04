1328c0de19c388a5003acb8f8b67aede21ccadce
zrebhi <zakaria.rebhi@gmail.com>
2024-04-04 19:36:24 +0200

    Blockchain implementation

--- website/django_project/tournaments/blockchain.py
+++ website/django_project/tournaments/blockchain.py
@@ -19,187 +19,187 @@ my_account = account.address
 
 # Charger l'ABI du smart contract
 contract_abi = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tournamentId",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "_participant1",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_participant2",
-				"type": "string"
-			},
-			{
-				"internalType": "int256",
-				"name": "_scoreParticipant1",
-				"type": "int256"
-			},
-			{
-				"internalType": "int256",
-				"name": "_scoreParticipant2",
-				"type": "int256"
-			}
-		],
-		"name": "addMatch",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tournamentId",
-				"type": "uint256"
-			}
-		],
-		"name": "closeTournament",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string[]",
-				"name": "_participants",
-				"type": "string[]"
-			}
-		],
-		"name": "createTournament",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [],
-		"name": "ErrorListOfParticipantEmpty",
-		"type": "error"
-	},
-	{
-		"inputs": [],
-		"name": "ErrorOnlyOwnerFunction",
-		"type": "error"
-	},
-	{
-		"inputs": [],
-		"name": "ErrorTournamentAlreadyClosed",
-		"type": "error"
-	},
-	{
-		"inputs": [],
-		"name": "ErrorTournamentIsClosed",
-		"type": "error"
-	},
-	{
-		"inputs": [],
-		"name": "ErrorTournamentNotExist",
-		"type": "error"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tournamentId",
-				"type": "uint256"
-			}
-		],
-		"name": "getTournamentMatches",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "participant1",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "participant2",
-						"type": "string"
-					},
-					{
-						"internalType": "int256",
-						"name": "scoreParticipant1",
-						"type": "int256"
-					},
-					{
-						"internalType": "int256",
-						"name": "scoreParticipant2",
-						"type": "int256"
-					}
-				],
-				"internalType": "struct TournamentContract.Match[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_tournamentId",
-				"type": "uint256"
-			}
-		],
-		"name": "getTournamentParticipants",
-		"outputs": [
-			{
-				"internalType": "string[]",
-				"name": "",
-				"type": "string[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "nextTournamentId",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tournamentId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "_participant1",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "_participant2",
+                "type": "string"
+            },
+            {
+                "internalType": "int256",
+                "name": "_scoreParticipant1",
+                "type": "int256"
+            },
+            {
+                "internalType": "int256",
+                "name": "_scoreParticipant2",
+                "type": "int256"
+            }
+        ],
+        "name": "addMatch",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tournamentId",
+                "type": "uint256"
+            }
+        ],
+        "name": "closeTournament",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string[]",
+                "name": "_participants",
+                "type": "string[]"
+            }
+        ],
+        "name": "createTournament",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "ErrorListOfParticipantEmpty",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ErrorOnlyOwnerFunction",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ErrorTournamentAlreadyClosed",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ErrorTournamentIsClosed",
+        "type": "error"
+    },
+    {
+        "inputs": [],
+        "name": "ErrorTournamentNotExist",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tournamentId",
+                "type": "uint256"
+            }
+        ],
+        "name": "getTournamentMatches",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "string",
+                        "name": "participant1",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "participant2",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "int256",
+                        "name": "scoreParticipant1",
+                        "type": "int256"
+                    },
+                    {
+                        "internalType": "int256",
+                        "name": "scoreParticipant2",
+                        "type": "int256"
+                    }
+                ],
+                "internalType": "struct TournamentContract.Match[]",
+                "name": "",
+                "type": "tuple[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_tournamentId",
+                "type": "uint256"
+            }
+        ],
+        "name": "getTournamentParticipants",
+        "outputs": [
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "nextTournamentId",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ]
 
 # Adresse du smart contract déployé
@@ -207,6 +207,7 @@ contract_address = '0x1ee56BDdE0256EA994B36CD48b090Fa2fb9444f7'
 
 # Créer un objet de contrat
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
 
 # Exemple : Créer un nouveau tournoi
 def create_blockchain_tournament(participants):
@@ -231,12 +232,12 @@ def create_blockchain_tournament(participants):
         return None
 
 
-
 # Exemple : Ajouter un match à un tournoi existant
-def add_match_to_tournament(tournament_id, participant1, participant2, winner):
+def add_match_to_tournament(tournament_id, participant1, participant2, score1, score2):
     try:
         # Appel de la fonction de contrat pour ajouter un match au tournoi avec les détails donnés
-        tx_hash = contract.functions.addMatch(tournament_id, participant1, participant2, score1, score2).transact({'from': my_account})
+        tx_hash = (contract.functions.addMatch(tournament_id, participant1, participant2, score1, score2).
+                   transact({'from': my_account}))
 
         # Attendre la confirmation de la transaction
         receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
@@ -250,25 +251,27 @@ def add_match_to_tournament(tournament_id, participant1, participant2, winner):
         print("Erreur lors de l'ajout du match au tournoi :", e)
 
 # Exemple : Clôturer un tournoi existant
+
+
 def close_tournament(tournament_id):
     try:
         # Appel de la fonction de contrat pour fermer le tournoi spécifié
         tx_hash = contract.functions.closeTournament(tournament_id).transact({'from': my_account})
-		# Attendre la confirmation de la transaction
+        # Attendre la confirmation de la transaction
         receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
         # Vérifier le statut de la transaction
         if receipt.status == 1:
             print("Le tournoi a été clôturé avec succès !")
-			print_etherscan_transaction_url(tx_hash)
-		else:
+            print_etherscan_transaction_url(tx_hash)
+        else:
             print("La clôture du tournoi a échoué.")
     except Exception as e:
         print("Erreur lors de la clôture du tournoi :", e)
 
 
 # Utilisation des fonctions
-        
+
 
 # tournament_id = create_tournament(["Participant 1", "Participant 2", "Participant 3", "Participant 4"])
 # print(tournament_id)
@@ -284,9 +287,15 @@ def print_etherscan_transaction_url(tx_hash):
     etherscan_url = 'https://sepolia.etherscan.io/tx/'
     print("Transaction URL:", etherscan_url + tx_hash.hex())
 
-def set_tournament_in_blockchain(tournament, participantsArray):
-	tournament_id = create_blockchain_tournament(participantsArray) # Rajouter None dans l'array
-	for match in tournament.matches.all():
-		add_match_to_tournament(tournament_id, match.participant1, match.participant2, match.scoreParticipant1, match.scoreParticipant2)
-	close_tournament(tournament_id)
-	print(contract.functions.getTournamentMatches(tournament_id).call())
+
+def set_tournament_in_blockchain(tournament, participants_array):
+    tournament_id = create_blockchain_tournament(participants_array)
+    print(f"tournament_id: {tournament_id}")
+    print(f"array: {participants_array}")
+    for round in tournament.rounds.all():
+        for match in round.matches.all():
+            print(f"adding match: {match}, of round {round} in the blockchain")
+            add_match_to_tournament(tournament_id, match.player1.username, match.player2.username,
+                                    match.player1_score1, match.player2_score)
+    close_tournament(tournament_id)
+    print(contract.functions.getTournamentMatches(tournament_id).call())
