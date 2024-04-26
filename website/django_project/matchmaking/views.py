98087bdaad8092b7cf1bc1020149e56aa84cd692
zrebhi <zakaria.rebhi@gmail.com>
2024-04-26 17:18:27 +0200

    Prevented users from entering a game if they are already in a tournament or in a matchmaking queue

--- website/django_project/matchmaking/views.py
+++ website/django_project/matchmaking/views.py
@@ -17,9 +17,13 @@ def join_queue(request):
     if request.user.session_id is not None:
         return JsonResponse({'status': 'error', 'message': 'User already in a game'}, status=400)
 
+    if request.user.tournament_id is not None:
+        return JsonResponse({'status': 'error', 'message': 'User already in a tournament'}, status=400)
+
     QueueEntry.objects.create(user=request.user)
 
     return JsonResponse({'status': 'success', 'message': 'Successfully joined the queue'})
+
 
 @login_required
 def local_game(request):
@@ -27,6 +31,10 @@ def local_game(request):
         return JsonResponse({'status': 'error', 'message': 'User not authenticated'}, status=400)
     if request.user.session_id is not None:
         return JsonResponse({'status': 'error', 'message': 'User already in a game'}, status=400)
+    if QueueEntry.objects.filter(user=request.user).exists():
+        return JsonResponse({'status': 'error', 'message': 'User already in queue'}, status=400)
+    if request.user.tournament_id is not None:
+        return JsonResponse({'status': 'error', 'message': 'User already in a tournament'}, status=400)
 
     session = GameSession.objects.create(player1=request.user, player2=request.user, mode='local')
     create_game_instance(session.id)
