a64e9fde1e77ba6ceee48a9e4ce314fd9a725a66
zrebhi <zakaria.rebhi@gmail.com>
2024-04-29 19:32:33 +0200

    threeJS update

--- website/django_project/pong_app/static/pong_app/js/threejs.js
+++ website/django_project/pong_app/static/pong_app/js/threejs.js
@@ -1,175 +1,235 @@
-import { gameData } from './pong.js';
-import { getPauseMessage } from './draw.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
 
-let scene, camera, ball, ballLight, paddle1, paddle2;
-let textMeshes = {};
-let fontLoaded = false;
-const fontLoader = new THREE.FontLoader();
-let font;
+import { gameData } from "./pong.js";
 
-fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (loadedFont) {
-    font = loadedFont;
-    fontLoaded = true;
-});
+let scene,
+  camera,
+  ball,
+  ballLight,
+  paddle1,
+  paddle2,
+  ground,
+  contour,
+  middle,
+  edges1,
+  edges2;
 
 export function setupCanvas() {
-    if (!gameData.renderer) {
-        const canvas = document.createElement('canvas');
-        canvas.width = gameData.canvasContainerWidth;
-        canvas.height = gameData.canvasContainerHeight;
-        const canvasContainer = document.getElementById('canvasContainer');
-        
-        if (canvasContainer) {
-            canvasContainer.appendChild(canvas);
-            scene = new THREE.Scene();
-            camera = new THREE.PerspectiveCamera(75, gameData.canvasContainerWidth / gameData.canvasContainerHeight, 0.1, 1000);
-            camera.position.set(600, 450, 570);
-            camera.scale.y = -1;
-        
-            gameData.renderer = new THREE.WebGLRenderer({ canvas });
-            gameData.renderer.setSize(gameData.canvasContainerWidth, gameData.canvasContainerHeight);
+  if (!gameData.renderer) {
+    const canvas = document.createElement("canvas");
+    const canvasContainer = document.getElementById("canvasContainer");
 
-            addElementsToScene();
-        }
+    // Initialize camera
+    camera = new THREE.PerspectiveCamera(
+      80,
+      gameData.canvasContainerWidth / gameData.canvasContainerHeight,
+      1,
+      10000
+    );
+    camera.position.set(600, 900, 650);
+    camera.scale.y = -1;
+    camera.rotation.set(-0.4, 0, 0);
+
+    // Update canvas size based on container size
+    if (!gameData.scoreDisplay) {
+      gameData.scoreDisplay = document.createElement("div");
+      gameData.scoreDisplay.id = "scoreDisplay";
+      gameData.scoreDisplay.textContent = `${gameData.player1} : ${gameData.paddle1.score} - ${gameData.player2} : ${gameData.paddle2.score}`;
+
+      if (canvasContainer) {
+        canvasContainer.appendChild(gameData.scoreDisplay);
+      }
     }
+
+    if (canvasContainer) {
+      canvasContainer.appendChild(canvas);
+      scene = new THREE.Scene();
+
+      gameData.renderer = new THREE.WebGLRenderer({ canvas });
+      gameData.renderer.setPixelRatio(window.devicePixelRatio); // Consider device pixel ratio
+      gameData.renderer.setSize(
+        (gameData.canvasContainerWidth * window.devicePixelRatio) / 2,
+        (gameData.canvasContainerHeight * window.devicePixelRatio) / 2
+      );
+      gameData.renderer.setClearColor(0x000000, 0);
+      gameData.renderer.antialias = true;
+
+      addElementsToScene();
+    }
+  }
 }
 
-
 function addElementsToScene() {
-    addPaddles();
-    addBall();
-    addLights();
-    addTextMeshes();
+  addGround();
+  addPaddles();
+  addBall();
+  addLights();
+  addContour();
 }
 
 function addPaddles() {
-    const paddleGeometry = new THREE.BoxGeometry(20, 90, 10);
-    const paddleMaterial = new THREE.MeshPhongMaterial({color: 0xffffff});
-    paddle1 = new THREE.Mesh(paddleGeometry, paddleMaterial);
-    paddle2 = new THREE.Mesh(paddleGeometry, paddleMaterial);
-    
-    paddle1.position.set(gameData.paddle1.xpos, gameData.paddle1.ypos + 40, 0);
-    paddle2.position.set(gameData.paddle2.xpos, gameData.paddle2.ypos + 40, 0);
-    scene.add(paddle1, paddle2);
+  const paddleGeometry = new THREE.BoxGeometry(20, 90, 20);
+  const paddleMaterial = new THREE.MeshStandardMaterial({ color: 0xffacffac });
+  paddle1 = new THREE.Mesh(paddleGeometry, paddleMaterial);
+  paddle2 = new THREE.Mesh(paddleGeometry, paddleMaterial);
+
+  // Set paddle positions
+  paddle1.position.set(gameData.paddle1.xpos, gameData.paddle1.ypos + 40, 10);
+  paddle2.position.set(gameData.paddle2.xpos, gameData.paddle2.ypos + 40, 10);
+
+  // Add paddles to the scene
+  scene.add(paddle1, paddle2);
+
+  // Edge geometry and material
+  const edgeGeometry = new THREE.EdgesGeometry(paddleGeometry);
+  const edgeMaterial = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+    linewidth: 2,
+  });
+
+  // Create edge meshes
+  edges1 = new THREE.LineSegments(edgeGeometry, edgeMaterial);
+  edges2 = new THREE.LineSegments(edgeGeometry, edgeMaterial);
+
+  // Align edges with the paddles
+  edges1.position.copy(paddle1.position);
+  edges2.position.copy(paddle2.position);
+
+  // Add edges to the scene
+  scene.add(edges1, edges2);
 }
-
 function addBall() {
-    const ballGeometry = new THREE.SphereGeometry(9, 32, 32);
-    const ballMaterial = new THREE.MeshPhongMaterial({color: 0x800080, emissive: 0x800080, transparent: true, opacity: 1});
-    ball = new THREE.Mesh(ballGeometry, ballMaterial);
-    scene.add(ball);
+  const ballGeometry = new THREE.SphereGeometry(10, 32, 32);
+  const ballMaterial = new THREE.MeshStandardMaterial({
+    color: 0xffff,
+  });
+  ball = new THREE.Mesh(ballGeometry, ballMaterial);
 
-    ballLight = new THREE.PointLight(0x800080, 30, 10000);
-    ballLight.position.set(gameData.ball.xpos, gameData.ball.ypos, -10);
-    scene.add(ballLight);
+  ball.castShadow = true;
+  ball.receiveShadow = true;
+  scene.add(ball);
+
+  ballLight = new THREE.PointLight(0xfcafff, 5, 0);
+  ballLight.castShadow = true;
+  scene.add(ballLight);
 }
 
 function addLights() {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
 }
 
-function addTextMeshes() {
-    updateText('status', gameData?.status, 500, 100);
-    updateText('player1Score', `${gameData?.player1} : ${gameData?.paddle1?.score}`, 50, 100);
-    updateText('player2Score', `${gameData?.player2} : ${gameData?.paddle2?.score}`, 950, 100);
+function addGround() {
+  const groundGeometry = new THREE.BoxGeometry(1200, 900, 3);
+  const groundMaterial = new THREE.MeshStandardMaterial({
+    color: 0x4c3d,
+  });
+  ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  ground.position.set(600, 450, -5);
+
+  scene.add(ground);
 }
 
-function updateText(id, string, x, y) {
-    if (!fontLoaded) {
-        setTimeout(() => {
-            updateText(id, string, x, y);
-        }, 100);
-        return;
-    }
+function addContour() {
+  const contourGeometry = new THREE.BoxGeometry(1210, 920, 1);
+  const contourMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  contour = new THREE.Mesh(contourGeometry, contourMaterial);
+  contour.position.set(600, 450, -7);
 
-    if (!string) return ;
+  const middleGeometry = new THREE.BoxGeometry(10, 910, 1);
+  const middleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  middle = new THREE.Mesh(middleGeometry, middleMaterial);
+  middle.position.set(600, 450, -4);
 
-    const existingTextMesh = textMeshes[id]?.mesh;
-
-    const textGeometry = new THREE.TextGeometry(string, {
-        font: font,
-        size: 30,
-        height: 2,
-        curveSegments: 12,
-        bevelThickness: 10,
-        bevelSize: 8,
-        bevelOffset: 0,
-        bevelSegments: 5,
-    });
-    textGeometry.scale(1, -1, 1);
-    const textMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0xffffff });
-    const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(x, y, 0);
-    scene.add(textMesh);
-
-    if (existingTextMesh) {
-        scene.remove(existingTextMesh);
-    }
-
-    textMeshes[id] = { mesh: textMesh, string: string };
+  scene.add(middle);
+  scene.add(contour);
 }
 
 export function draw3dCanvas() {
-    setupCanvas();
-    function animate() {
-        gameData.animationid = requestAnimationFrame(animate);
-        updateText('status', gameData?.status, 500, 100);
-        updateText('player1Score', `${gameData?.player1} : ${gameData?.paddle1?.score}`, 50, 100);
-        updateText('player2Score', `${gameData?.player2} : ${gameData?.paddle2?.score}`, 950, 100);
+  setupCanvas();
+  (function animate() {
+    gameData.animationid = requestAnimationFrame(animate);
 
-        if (!gameData.winner && !gameData.forfeitMessage) {
-            updateGameElementsPosition();
-            gameData.renderer?.render(scene, camera);
-        } else if (gameData.forfeitMessage || gameData.winner) {
-            handleGameEnd();
-            return ;
-        } else {
-            gameData.renderer?.render(scene, camera);
-        }
+    if (!gameData.winner && !gameData.forfeitMessage) {
+      updateGameElementsPosition();
+      updateScoreDisplay();
+      gameData.renderer?.render(scene, camera);
+    } else if (gameData.forfeitMessage || gameData.winner) {
+      handleGameEnd();
+      return;
+    } else {
+      gameData.renderer?.render(scene, camera);
     }
-    animate();
-}
-
-function updateGameElementsPosition() {
-    ball.position.set(gameData.ball?.xpos, gameData.ball?.ypos, 0);
-    ballLight.position.set(gameData.ball?.xpos, gameData.ball?.ypos, -10);
-    paddle1.position.set(gameData.paddle1?.xpos, gameData.paddle1?.ypos + 40, 0);
-    paddle2.position.set(gameData.paddle2?.xpos, gameData.paddle2?.ypos + 40, 0);
+  })();
 }
 
 function handleGameEnd() {
-    if (gameData.forfeitMessage) {
-        updateText('forfeitMessage', gameData.forfeitMessage, 500, 500);
-    } else if (gameData.winner) {
-        updateText('winner', `Winner: ${gameData.winner}`, 500, 500);
-    }
-    gameData.renderer?.render(scene, camera);
+  if (gameData.forfeitMessage) {
+    gameData.scoreDisplay.textContent = `${gameData.forfeitMessage}`;
+  }
 
-    endGame();
+  endGame();
+}
+
+function updateGameElementsPosition() {
+  ball.position.set(gameData.ball?.xpos, gameData.ball?.ypos, 10);
+  ballLight.position.set(gameData.ball?.xpos, gameData.ball?.ypos, -10);
+  paddle1.position.set(gameData.paddle1?.xpos, gameData.paddle1?.ypos + 40, 10);
+  edges1.position.set(gameData.paddle1?.xpos, gameData.paddle1?.ypos + 40, 10);
+  paddle2.position.set(gameData.paddle2?.xpos, gameData.paddle2?.ypos + 40, 10);
+  edges2.position.set(gameData.paddle2?.xpos, gameData.paddle2?.ypos + 40, 10);
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let previousScore = null;
+
+function updateScoreDisplay() {
+  const currentScore = `${gameData.player1} : ${gameData.paddle1.score} - ${gameData.player2} : ${gameData.paddle2.score}`;
+
+  if (gameData.paddle1.score !== 0 || gameData.paddle2.score !== 0) {
+    gameData.scoreDisplay.textContent = currentScore;
+    if (currentScore !== previousScore) {
+      ballLight.color.set(getRandomColor());
+      console.log("Le score a été mis à jour :", currentScore);
+      previousScore = currentScore;
+    }
+  }
 }
 
 export function endGame() {
-    cancelAnimationFrame(gameData.animationid);
-    
-    // Supprimer les éléments de la scène
-    if (scene && ball && paddle1 && paddle2 && ballLight)
-        scene.remove(ball, paddle1, paddle2, ballLight);
-    
-    // Supprimer les éléments de texte de la scène
-    for (const key in textMeshes) {
-        if (textMeshes.hasOwnProperty(key)) {
-            const textMesh = textMeshes[key].mesh;
-            if (textMesh && scene)
-                scene.remove(textMesh);
-        }
-    }
-    
-    // Réinitialiser les variables à null
-    scene = camera = ball = ballLight = paddle1 = paddle2 = gameData.animationid = null;
+  cancelAnimationFrame(gameData.animationid);
 
-    if (gameData.renderer) {
-        gameData.renderer.dispose();
-        // renderer.forceContextLoss()
-    }
+  if (scene && ball && paddle1 && paddle2 && ballLight && ground)
+    scene.remove(ball, paddle1, paddle2, ballLight, ground);
+
+  scene =
+    camera =
+    ball =
+    ballLight =
+    paddle1 =
+    paddle2 =
+    ground =
+    contour =
+    middle =
+    edges1 =
+    edges2 =
+    gameData.animationid =
+      null;
+
+  if (gameData.renderer) {
+    gameData.renderer.dispose();
+  }
+
+  if (gameData.scoreDisplay) {
+    gameData.scoreDisplay.remove();
+    gameData.scoreDisplay = null;
+  }
 }
