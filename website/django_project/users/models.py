3cba8e7ad2f96a265f7227ea9e227e82fcb1070d
zrebhi <zakaria.rebhi@gmail.com>
2024-05-13 19:03:21 +0200

    updated tournament change alias front

--- website/django_project/users/models.py
+++ website/django_project/users/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
 
     def save(self, *args, **kwargs):
         if not self.alias:
-            self.alias = self.username[:20]  # Prend les 10 premiers caractères du username
+            self.alias = self.username[:20]  # Prend les 20 premiers caractères du username
         super(CustomUser, self).save(*args, **kwargs)
 
     def __str__(self):
